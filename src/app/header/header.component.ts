e74d22ead2b8801ecd35a379357d4f29e60bdfd1
NilsHolger <nils.holger.naegele@gmail.com>
2017-10-15 23:04:46 +0200

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, NavigationStart, RoutesRecognized } from '@angular/router';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  hideSettings;
-  constructor() { }
+export class HeaderComponent {
+  hideSettings: boolean;
 
-  ngOnInit() {
-  }
+  constructor(private router: Router, private route: ActivatedRoute) {
+        this.router.events.map(event => event instanceof NavigationStart)
+                          .subscribe(() => {
+                            this.hideSettings = ('/news' !== this.router.url) ? true : false;
+                          });
+   }
 
 }
