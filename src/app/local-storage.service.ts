24069c78004f44b0489c559b0ccb0ea5c75bd7d5
NilsHolger <nils.holger.naegele@gmail.com>
2017-10-15 23:32:27 +0200

    services

--- src/app/local-storage.service.ts
+++ src/app/local-storage.service.ts
@@ -3,6 +3,36 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class LocalStorageService {
 
-  constructor() { }
+  localStorage: any;
 
+  constructor() {
+    if (!localStorage) {
+      throw new Error('Your current browser does not support Local Storage');
+    }
+    this.localStorage = localStorage;
+  }
+
+  set(key: string, value: string): void {
+    this.localStorage[key] = value;
+  }
+
+  get(key: string): string {
+    return this.localStorage[key] || false;
+  }
+
+  setArray(key: string, value: any): void {
+    this.localStorage[key] = JSON.stringify(value);
+  }
+
+  getArray(key: string): any {
+    return JSON.parse(this.localStorage[key] || '[]');
+  }
+
+  setObject(key: string, value: any): void {
+    this.localStorage[key] = JSON.stringify(value);
+  }
+
+  getObject(key: string): any {
+    return JSON.parse(this.localStorage[key] || '{}');
+  }
 }
--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class NewsApiService {
+  baseUrl: string;
+  apiKey: string;
 
-  constructor() { }
+
+  constructor(private http: Http) {
+    this.baseUrl = 'https://newsapi.org/v1';
+    this.apiKey = '7ac0b85b08624adaa00f7f4f155412e0';
+   }
+
+   fetchSources(): Observable<any> {
+     return this.http.get(`${this.baseUrl}/sources`)
+                     .map(response => response.json());
+   }
+
+   fetchArticles(source: string): Observable<any> {
+     return this.http.get(`${this.baseUrl}/articles?source=${source}&apiKey=${this.apiKey}`)
+                     .map(response => response.json());
+   }
 
 }
